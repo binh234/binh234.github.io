7b5c89a8dc8751c125fd0149262a2a3b1c52e97a
Binh Le <binhnd234@gmail.com>
2023-03-09 09:10:37 +0700

    Add Skype icon

--- src/components/icons/formattedIcon.js
+++ src/components/icons/formattedIcon.js
@@ -14,6 +14,7 @@ import {
   IconLocation,
   IconLogo,
   IconPlayStore,
+  IconSkype,
   IconStar,
   IconTwitter,
   IconZap,
@@ -47,8 +48,10 @@ const FormattedIcon = ({ name }) => {
       return <IconLogo />;
     case "PlayStore":
       return <IconPlayStore />;
-    case "Star":
-      return <IconStar />;
+    case "Skype":
+      return <IconSkype />;
+      case "Star":
+        return <IconStar />;
     case "Twitter":
       return <IconTwitter />;
     case "Zap":
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -12,6 +12,7 @@ export { default as IconLoader } from "./loader";
 export { default as IconLocation } from "./location";
 export { default as IconLogo } from "./logo";
 export { default as IconPlayStore } from "./playstore";
+export { default as IconSkype } from "./skype";
 export { default as IconStar } from "./star";
 export { default as IconTwitter } from "./twitter";
 export { default as IconYouTube } from "./youtube";
